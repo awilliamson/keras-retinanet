f74c43b3ec6b07e2ef5659d1f8ea021ff8fe20fc
Ashley Williamson <awilliamson@lincoln.ac.uk>
2018-02-07 23:24:51 +0000

    Switch to Yapsy plugin manager
    Handles importing in an error-free way, extensible.

---
+++ keras_retinanet/utils/plugin.py
@@ -0,0 +1,13 @@
+from yapsy.IPlugin import IPlugin
+
+
+class DatasetPlugin(IPlugin):
+    def __init__(self):
+        super(DatasetPlugin, self).__init__()
+
+
+    def parser_args(self):
+        pass
+
+    def get_generator(self):
+        pass
